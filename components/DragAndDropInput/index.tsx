9d34102510da4888fa5efaff968866e27369c9e6
chidi-locusnine <chidiebere@locusnine.com>
2022-06-29 03:55:52 +0100

    --feat: display native functions in a list

--- components/DragAndDropInput/index.tsx
+++ components/DragAndDropInput/index.tsx
@@ -94,12 +94,15 @@ function DragAndDropInput({
             {solFunctions.map((func, index) => {
               if (index === solFunctions.length - 1)
                 return (
-                  <li className="px-6 py-2 w-full rounded-b-lg">
+                  <li key={index} className="px-6 py-2 w-full rounded-b-lg">
                     {index + 1}. {func}
                   </li>
                 );
               return (
-                <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                <li
+                  key={index}
+                  className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg"
+                >
                   {index + 1}. {func}
                 </li>
               );
