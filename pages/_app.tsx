c5734bf08c6e6c03dc8dd9fc93a4ef1eea60220e
davien21 <chidiebereekennia@gmail.com>
2022-07-11 06:46:31 +0100

    --fix favicon logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <ToastContainer position="top-center" autoClose={5000} />
       <Head>
-        <link rel="icon" href="./favicon.svg" />
+        <link rel="icon" href="./favicon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta
